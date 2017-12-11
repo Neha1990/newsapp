0dc1b9993dc06cda81f978c71a747dd6514bfc55
NehaKhairnar <neha@qpair.io>
2017-12-10 21:44:30 -0500

    Working with CRUD

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,16 @@
+
+class Body extends React.Component {
+  render() {
+    return (
+		
+	
+		<div>
+		<h1>List of news reports</h1>
+		
+		</div>
+  
+  
+  
+	);
+  }
+}
---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,10 @@
+
+
+
+class Header extends React.Component {
+  render() {
+    return (
+		<h1>Welcome to Hackers News</h1>
+	);
+  }
+}
